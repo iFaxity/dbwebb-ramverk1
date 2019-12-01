75c9deee5163a4815af80a5507f9b69349f3e8f5
Christian Norrman <christian@faxity.se>
2019-12-01 23:01:13 +0100

    Added Weather controller & views

---
+++ config/api.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Config file for storing API keys
+ */
+return [
+    // Use for styling the menu
+    "ipstack" => "e0318d38996064dac9467a1dd467e419",
+    "darksky" => "fa2210eb4890c8732ccff2ce3ed8b96c",
+];
