4ac3b2d6e82ce3a48b65c49cb55f8578d4b7a878
Christian Norrman <christian@faxity.se>
2019-12-09 23:35:05 +0100

    Swapped DIMagic for DISorcery

--- htdocs/index.php
+++ htdocs/index.php
@@ -4,22 +4,21 @@
  */
 
 // Were are all the files?
-define("ANAX_INSTALL_PATH", realpath(__DIR__ . "/.."));
+define('ANAX_INSTALL_PATH', realpath(__DIR__ . '/..'));
 
 // Set development/production environment and error reporting
-require ANAX_INSTALL_PATH . "/config/commons.php";
+require ANAX_INSTALL_PATH . '/config/commons.php';
 
 // Get the autoloader by using composers version.
-require ANAX_INSTALL_PATH . "/vendor/autoload.php";
+require ANAX_INSTALL_PATH . '/vendor/autoload.php';
 
 // Add all framework services to $di
-$di = new \Anax\DI\DIMagic();
-$di->loadServices(ANAX_INSTALL_PATH . "/config/di");
+$di = new \Faxity\DI\DISorcery(ANAX_INSTALL_PATH);
+$di->initialize('config/sources.php');
 
 // Send the response that the router returns from the route handler
-$di->response->send(
-    $di->router->handle(
-        $di->request->getRoute(),
-        $di->request->getMethod()
-    )
-);
+$method = $di->request->getMethod();
+$route = $di->request->getRoute();
+$router = $di->router->handle($route, $method);
+
+$di->response->send($router);
